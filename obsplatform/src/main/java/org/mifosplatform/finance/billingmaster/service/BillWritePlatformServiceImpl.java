2d18943004703c3f4f1823e843f11de54f6338e8
Ranjith15 <ranjithworld150@gmail.com>
2014-10-08 17:22:34 +0530

    changes in managetables

--- obsplatform/src/main/java/org/mifosplatform/finance/billingmaster/service/BillWritePlatformServiceImpl.java
+++ obsplatform/src/main/java/org/mifosplatform/finance/billingmaster/service/BillWritePlatformServiceImpl.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
