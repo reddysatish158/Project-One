7379834f7c97ec48ad3b68aabd49c20fbbd1b1a1
Rakesh <rakesh.adlagatta@gmail.com>
2014-10-30 19:07:08 +0530

    scripts changes

--- obs-db/migrations/core_db/V150__Filters_groups_permissions_added.sql
+++ obs-db/migrations/core_db/V150__Filters_groups_permissions_added.sql
@@ -5,3 +5,12 @@ update m_permission set code='CREATE_GROUPSPROVISION',entity_name='GROUPSPROVISI
 update m_permission set code='CREATE_GROUPSDETAILS',entity_name='GROUPSDETAILS' where code='CREATE_GROUPS';
 
 update m_permission set code='READ_GROUPSDETAILS', entity_name='GROUPSDETAILS' where code='READ_GROUPS';
+
+
+insert IGNORE into m_permission values(null, 'organisation', 'CREATE_VOUCHER', 'VOUCHER', 'CREATE', '0');
+insert IGNORE into m_permission values(null, 'organisation', 'PROCESS_VOUCHER', 'VOUCHER', 'PROCESS', '1');
+insert IGNORE into m_permission values(null, 'organisation', 'READ_VOUCHER', 'VOUCHER', 'READ', '1');
+
+delete from m_permission where entity_name Like '%RANDOMGENERATOR%';
+
+insert IGNORE into c_configuration VALUES (null,'OSD_ProvisioningSystem',1,'Comvenient');
