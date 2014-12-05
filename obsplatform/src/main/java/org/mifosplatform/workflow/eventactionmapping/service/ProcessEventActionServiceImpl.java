cab05164edc5cd685c660b9e9930d76866bc7ae8
Ranjith15 <ranjithworld150@gmail.com>
2014-12-05 11:24:30 +0530

    event action

--- obsplatform/src/main/java/org/mifosplatform/workflow/eventactionmapping/service/ProcessEventActionServiceImpl.java
+++ obsplatform/src/main/java/org/mifosplatform/workflow/eventactionmapping/service/ProcessEventActionServiceImpl.java
@@ -4,8 +4,11 @@ package org.mifosplatform.workflow.eventactionmapping.service;
 import java.util.Date;
 import java.util.List;
 
+import org.mifosplatform.finance.billingmaster.api.BillingMasterApiResourse;
+import org.mifosplatform.finance.billingmaster.service.BillMasterWritePlatformService;
 import org.mifosplatform.finance.billingorder.service.InvoiceClient;
 import org.mifosplatform.infrastructure.core.api.JsonCommand;
+import org.mifosplatform.infrastructure.core.data.CommandProcessingResult;
 import org.mifosplatform.infrastructure.core.serialization.FromJsonHelper;
 import org.mifosplatform.portfolio.association.data.HardwareAssociationData;
 import org.mifosplatform.portfolio.association.service.HardwareAssociationReadplatformService;
@@ -38,13 +41,16 @@ public class ProcessEventActionServiceImpl implements ProcessEventActionService 
 	private final ProcessRequestRepository processRequestRepository;
     private final OrderWritePlatformService orderWritePlatformService;
     private final HardwareAssociationReadplatformService hardwareAssociationReadplatformService;
+    private final BillMasterWritePlatformService billMasterWritePlatformService;
+    private final BillingMasterApiResourse billingMasterApiResourse;
     
  
 
 	@Autowired
 	public ProcessEventActionServiceImpl(final EventActionRepository eventActionRepository,final FromJsonHelper fromJsonHelper,
 			final OrderWritePlatformService orderWritePlatformService,final InvoiceClient invoiceClient,
-			final ProcessRequestRepository processRequestRepository,final HardwareAssociationReadplatformService hardwareAssociationReadplatformService)
+			final ProcessRequestRepository processRequestRepository,final HardwareAssociationReadplatformService hardwareAssociationReadplatformService,
+			final BillMasterWritePlatformService billMasterWritePlatformService,final BillingMasterApiResourse billingMasterApiResourse)
 	{
 		this.invoiceClient=invoiceClient;
         this.fromApiJsonHelper=fromJsonHelper;
@@ -52,6 +58,9 @@ public class ProcessEventActionServiceImpl implements ProcessEventActionService 
         this.processRequestRepository=processRequestRepository;
         this.orderWritePlatformService=orderWritePlatformService;
         this.hardwareAssociationReadplatformService=hardwareAssociationReadplatformService;
+        this.billMasterWritePlatformService = billMasterWritePlatformService;
+        this.billingMasterApiResourse = billingMasterApiResourse;
+        
 	}
 	
 	@Override
@@ -94,10 +103,21 @@ public class ProcessEventActionServiceImpl implements ProcessEventActionService 
 				
 			case EventActionConstants.ACTION_INVOICE :
 				try{
+					CommandProcessingResult result = null;
 					parsedCommand = this.fromApiJsonHelper.parse(jsonObject);
 					command = JsonCommand.from(jsonObject,parsedCommand,this.fromApiJsonHelper,"CreateInvoice",eventActionData.getClientId(), null,
 						null,eventActionData.getClientId(), null, null, null,null, null, null,null);
-					this.invoiceClient.createInvoiceBill(command);
+					result=this.invoiceClient.createInvoiceBill(command);
+					if(result!=null){
+						command = JsonCommand.from(jsonObject,parsedCommand,this.fromApiJsonHelper,"BILLMASTER",eventActionData.getClientId(), null,
+								null,eventActionData.getClientId(), null, null, null,null, null, null,null);
+			           result = this.billMasterWritePlatformService.createBillMaster(command, command.entityId());
+				           if(result.resourceId() != null){
+				        	  this.billingMasterApiResourse.printInvoice(result.resourceId());
+				        	  this.billingMasterApiResourse.sendBillPathToMsg(result.resourceId());
+				           }
+					}
+					
 				}catch(Exception exception){
 					
 				}
