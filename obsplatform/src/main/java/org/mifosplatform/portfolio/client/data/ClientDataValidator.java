e1800484601565a12b0ef23b205222f9905589a8
ashokreddylingala <lingala.ashokreddy@gmail.com>
2014-12-09 18:47:22 +0530

    selfcareChanges

--- obsplatform/src/main/java/org/mifosplatform/portfolio/client/data/ClientDataValidator.java
+++ obsplatform/src/main/java/org/mifosplatform/portfolio/client/data/ClientDataValidator.java
@@ -42,7 +42,7 @@ public final class ClientDataValidator {
         this.fromApiJsonHelper = fromApiJsonHelper;
     }
 
-    public void validateForCreate(final String json) {
+    public void validateForCreate(final String json, boolean isSelfcareEnable) {
 
         if (StringUtils.isBlank(json)) { throw new InvalidJsonException(); }
 
@@ -140,7 +140,7 @@ public final class ClientDataValidator {
         }
         if (fromApiJsonHelper.parameterExists(ClientApiConstants.emailParamName, element)) {
             final String email = fromApiJsonHelper.extractStringNamed(ClientApiConstants.emailParamName, element);
-            
+       
             baseDataValidator.reset().parameter(ClientApiConstants.emailParamName).value(email).notNull();
                  
             if(email!=null){
@@ -150,6 +150,11 @@ public final class ClientDataValidator {
             
             }
             
+        }
+        
+        if (isSelfcareEnable) {
+        	final String email = fromApiJsonHelper.extractStringNamed(ClientApiConstants.emailParamName, element);
+        	baseDataValidator.reset().parameter(ClientApiConstants.emailParamName).value(email).notNull();
         }
 
         throwExceptionIfValidationWarningsExist(dataValidationErrors);
