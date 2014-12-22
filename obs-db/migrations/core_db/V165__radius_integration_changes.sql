b933f77711ffaa0bd28c49ad63b191933236c8d4
ashokreddylingala <lingala.ashokreddy@gmail.com>
2014-12-22 20:21:04 +0530

    b_pg table update

--- obs-db/migrations/core_db/V165__radius_integration_changes.sql
+++ obs-db/migrations/core_db/V165__radius_integration_changes.sql
@@ -6,3 +6,19 @@ update job_parameters set param_value='hugo' where param_name='Username';
 update job_parameters set param_value='hugoadmin' where param_name='Password';
 update job_parameters set param_value='http://v241.streamingmedia.is:8556/' where param_name='URL';
 SET SQL_SAFE_UPDATES = 1;
+
+Drop procedure IF EXISTS paymentDates; 
+DELIMITER //
+create procedure paymentDates() 
+Begin
+IF  EXISTS (
+     SELECT * FROM information_schema.COLUMNS
+     WHERE  COLUMN_NAME ='lastmodifiedby_id' 
+       and TABLE_NAME = 'b_paymentgateway'
+     and TABLE_SCHEMA = DATABASE())THEN
+ALTER TABLE b_paymentgateway modify COLUMN `lastmodifiedby_id` bigint(20) DEFAULT NULL after `created_date`;
+END IF;
+END //
+DELIMITER ;
+call paymentDates();
+Drop procedure IF EXISTS paymentDates;
