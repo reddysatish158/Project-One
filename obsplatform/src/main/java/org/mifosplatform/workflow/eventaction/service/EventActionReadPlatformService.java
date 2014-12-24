a74a434ebc9b87103462382fb8c971053f746e06
Rakesh <rakesh.adlagatta@gmail.com>
2014-12-24 14:10:35 +0530

    eventaction listing

--- obsplatform/src/main/java/org/mifosplatform/workflow/eventaction/service/EventActionReadPlatformService.java
+++ obsplatform/src/main/java/org/mifosplatform/workflow/eventaction/service/EventActionReadPlatformService.java
@@ -1,9 +1,15 @@
 package org.mifosplatform.workflow.eventaction.service;
 
+import org.mifosplatform.crm.clientprospect.service.SearchSqlQuery;
+import org.mifosplatform.crm.ticketmaster.data.ClientTicketData;
+import org.mifosplatform.infrastructure.core.service.Page;
+import org.mifosplatform.scheduledjobs.scheduledjobs.data.EventActionData;
 import org.mifosplatform.workflow.eventaction.data.VolumeDetailsData;
 
 public interface EventActionReadPlatformService {
 
 	VolumeDetailsData retrieveVolumeDetails(Long id);
+	
+	Page<EventActionData> retriveAllEventActions(SearchSqlQuery searchTicketMaster, String statusType);
 
 }
